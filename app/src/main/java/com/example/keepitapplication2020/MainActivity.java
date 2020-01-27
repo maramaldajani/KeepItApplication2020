536864e6ba10547a1939088c4df951b9c24f1c77
hp <maramaldajan@gmail.com>
2020-01-27 19:55:27 +0300

    test

---
+++ app/src/main/java/com/example/keepitapplication2020/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.keepitapplication2020;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+          DatabaseReference reference;
+        reference = FirebaseDatabase.getInstance().getReference("Database").child("Text");
+        reference.setValue("maram");
+    }
+}
